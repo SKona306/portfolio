cfd3db1892344092f577bf230bdc92e891ce2824
Shaun Kent <shaunkent81@gmail.com>
2021-06-14 19:53:12 -0700

    add fade in effect in js, add div for about me section

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function scrollAppear() {
+  const aboutMeText = document.querySelector(".aboutme-text");
+  const aboutMePosition = aboutMeText.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight / 1.3;
+
+  if(aboutMePosition < screenPosition) {
+    aboutMeText.classList.add("aboutme-appear");
+  };
+};
+
+window.addEventListener("scroll" ,scrollAppear);
